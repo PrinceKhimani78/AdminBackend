092920fd9e3b068011cee6a30e4905446e504212
Princekhimani78 <prince@mutanttechnologies.com>
2026-03-08 19:10:09 +0530

    chore: remove diagnostic /debug-routes endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -29,45 +29,6 @@ router.use('/lookup', lookupRoutes);
 // Multer error handler (must be after routes that use multer)
 router.use(handleMulterError);
 
-// Debug routes
-router.get('/debug-routes', (req, res) => {
-  const routes: any[] = [];
-
-  function print(path: any, layer: any) {
-    if (layer.route) {
-      layer.route.stack.forEach(print.bind(null, path.concat(split(layer.route.path))));
-    } else if (layer.name === 'router' && layer.handle.stack) {
-      layer.handle.stack.forEach(print.bind(null, path.concat(split(layer.regexp))));
-    } else if (layer.method) {
-      routes.push({
-        method: layer.method.toUpperCase(),
-        path: '/' + path.concat(split(layer.regexp)).filter(Boolean).join('/')
-      });
-    }
-  }
-
-  function split(thing: any) {
-    if (typeof thing === 'string') {
-      return thing.split('/');
-    } else if (thing.fast_slash) {
-      return '';
-    } else {
-      const match = thing.toString()
-        .replace('\\/?', '')
-        .replace('(?=\\/|$)', '')
-        .match(/^\/\^((?:\\[.*+?^${}()|[\]\\]|[^\\[.*+?^${}()|[\]\\])*)\$\//);
-      return match
-        ? match[1].replace(/\\(.)/g, '$1').split('/')
-        : '<complex:' + thing.toString() + '>';
-    }
-  }
-
-  // @ts-ignore
-  router.stack.forEach(print.bind(null, []));
-
-  res.json({ success: true, count: routes.length, routes });
-});
-
 // Health check
 router.get('/health', (req, res) => {
   res.json({
